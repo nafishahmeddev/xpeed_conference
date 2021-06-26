535c860a612f740bc5fc623814cd6079515ba673
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2021-06-26 12:38:37 +0530

    get usermedia resolved

--- src/App.js
+++ src/App.js
@@ -33,34 +33,44 @@ function App() {
 
 
     useEffect(function (){
-        navigator.getUserMedia = (
-            navigator.getUserMedia ||
-            navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia ||
-            navigator.msGetUserMedia
-        );
-        if (typeof navigator.mediaDevices.getUserMedia === 'undefined') {
-
-            navigator.getUserMedia({video: true, audio: false})
-                .then(stream => {
-                    my_stream = stream;
-                    my_vdo_ref.current.srcObject = stream;
-                    my_vdo_ref.current.play();
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        } else {
-            navigator.mediaDevices.getUserMedia({video: true, audio: false})
-                .then(stream => {
-                    my_stream = stream;
-                    my_vdo_ref.current.srcObject = stream;
-                    my_vdo_ref.current.play();
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
+        // Older browsers might not implement mediaDevices at all, so we set an empty object first
+        if (navigator.mediaDevices === undefined) {
+            navigator.mediaDevices = {};
         }
+
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+        if (navigator.mediaDevices.getUserMedia === undefined) {
+            navigator.mediaDevices.getUserMedia = function(constraints) {
+
+                // First get ahold of the legacy getUserMedia, if present
+                var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+                console.log(getUserMedia);
+                // Some browsers just don't implement it - return a rejected promise with an error
+                // to keep a consistent interface
+                if (!getUserMedia) {
+                    return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+                }
+
+                // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+                return new Promise(function(resolve, reject) {
+                    getUserMedia.call(navigator, constraints, resolve, reject);
+                });
+            }
+        }
+
+        navigator.mediaDevices.getUserMedia({video: true, audio: false})
+            .then(stream => {
+                my_stream = stream;
+                my_vdo_ref.current.srcObject = stream;
+                my_vdo_ref.current.play();
+            })
+            .catch(function (error) {
+                console.log(error.message);
+            });
+
     },[]);
     return (
         <div className="App">
